699090f0a420cc5348d664ab5d43d2f6b3e1f40b
Sir-Cow <16866289+Sir-Cow@users.noreply.github.com>
2026-05-25 12:20:10 +0100

    0.0.2
    - fix not being able to kick villagers out of beds
    - make villager head hitboxes larger when sleeping

---
+++ common/src/main/java/sircow/roomfortwo/mixin/EntityMixin.java
@@ -0,0 +1,32 @@
+package sircow.roomfortwo.mixin;
+
+import net.minecraft.core.Direction;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.npc.Villager;
+import net.minecraft.world.phys.AABB;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Entity.class)
+public class EntityMixin {
+    @Shadow private AABB bb;
+
+    @Inject(method = "getBoundingBox", at = @At("HEAD"), cancellable = true)
+    private void roomfortwo$getExpandedSleepingBox(CallbackInfoReturnable<AABB> cir) {
+        if ((Object) this instanceof Villager villager) {
+            if (!villager.isSleeping()) return;
+
+            AABB modifiedBox = this.bb.inflate(0.175D, 0.075D, 0.175D);
+
+            Direction sleepingDirection = villager.getBedOrientation();
+            if (sleepingDirection == null) return;
+
+            Direction sidewaysDirection = sleepingDirection.getCounterClockWise();
+
+            cir.setReturnValue(modifiedBox.move((sleepingDirection.getStepX() * 0.2D) + (sidewaysDirection.getStepX() * -0.2D), 0.2D, (sleepingDirection.getStepZ() * 0.2D) + (sidewaysDirection.getStepZ() * -0.2D)));
+        }
+    }
+}
---
+++ common/src/main/java/sircow/roomfortwo/mixin/VillagerMixin.java
@@ -0,0 +1,23 @@
+package sircow.roomfortwo.mixin;
+
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.npc.Villager;
+import net.minecraft.world.entity.player.Player;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Villager.class)
+public class VillagerMixin {
+    @Inject(method = "mobInteract", at = @At("HEAD"), cancellable = true)
+    private void roomfortwo$kickFromBed(Player player, InteractionHand hand, CallbackInfoReturnable<InteractionResult> cir) {
+        Villager villager = (Villager) (Object) this;
+        if (!villager.isSleeping()) return;
+        if (villager.level().isClientSide()) return;
+
+        villager.stopSleeping();
+        cir.setReturnValue(InteractionResult.SUCCESS);
+    }
+}
