97115270cc219c83f945db035f7cb429db42fa32
Sir-Cow <16866289+Sir-Cow@users.noreply.github.com>
2026-05-25 11:18:45 +0100

    0.0.2
    - fix not being able to kick villagers out of beds
    - make villager head hitboxes larger when sleeping

---
+++ common/src/main/java/sircow/roomfortwo/mixin/EntityMixin.java
@@ -0,0 +1,27 @@
+package sircow.roomfortwo.mixin;
+
+import net.minecraft.core.Direction;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.npc.villager.Villager;
+import net.minecraft.world.phys.AABB;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Entity.class)
+public class EntityMixin {
+    @Shadow private AABB bb;
+
+    @Inject(method = "getBoundingBox", at = @At("HEAD"), cancellable = true)
+    private void roomfortwo$getExpandedSleepingBox(CallbackInfoReturnable<AABB> cir) {
+        if ((Object) this instanceof Villager villager) {
+            if (!villager.isSleeping()) return;
+
+            AABB modifiedBox = this.bb.inflate(0.175D, 0.1D, 0.175D);
+            Direction sleepingDirection = villager.getBedOrientation();
+            cir.setReturnValue(modifiedBox.move(sleepingDirection.getStepX() * 0.2D, 0.0D, sleepingDirection.getStepZ() * 0.2D));
+        }
+    }
+}
