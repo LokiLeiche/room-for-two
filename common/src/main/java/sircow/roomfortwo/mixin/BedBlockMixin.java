39f4329ab5fd0e636a9a338a3a95a587c6c94d0e
Sir-Cow <16866289+Sir-Cow@users.noreply.github.com>
2026-06-06 01:14:28 +0100

    0.1.1
    - more specific mixin target for bed interaction

--- common/src/main/java/sircow/roomfortwo/mixin/BedBlockMixin.java
+++ common/src/main/java/sircow/roomfortwo/mixin/BedBlockMixin.java
@@ -1,45 +1,16 @@
 package sircow.roomfortwo.mixin;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BedBlock;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.properties.BedPart;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.level.block.state.properties.Property;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(BedBlock.class)
 public class BedBlockMixin {
-    @Inject(method = "useWithoutItem", at = @At("HEAD"), cancellable = true)
-    private void roomfortwo$use(BlockState state, Level level, BlockPos pos, Player player, BlockHitResult hitResult, CallbackInfoReturnable<InteractionResult> cir) {
-        if (level.isClientSide()) {
-            cir.setReturnValue(InteractionResult.CONSUME);
-            return;
-        }
-
-        BlockPos bedPos = pos;
-        BlockState bedState = state;
-
-        if (bedState.getValue(BlockStateProperties.BED_PART) != BedPart.HEAD) {
-            bedPos = bedPos.relative(bedState.getValue(BedBlock.FACING));
-            bedState = level.getBlockState(bedPos);
-
-            if (!bedState.is((BedBlock) (Object) this)) {
-                cir.setReturnValue(InteractionResult.CONSUME);
-                return;
-            }
-        }
-
-        player.startSleepInBed(bedPos).ifLeft(problem -> {
-            if (problem.message() != null) player.displayClientMessage(problem.message(), true);
-        });
-
-        cir.setReturnValue(InteractionResult.SUCCESS);
+    @Redirect(method = "useWithoutItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;getValue(Lnet/minecraft/world/level/block/state/properties/Property;)Ljava/lang/Comparable;", ordinal = 3))
+    private Comparable<?> roomfortwo$use(BlockState state, Property<?> property) {
+        return false;
     }
 }
